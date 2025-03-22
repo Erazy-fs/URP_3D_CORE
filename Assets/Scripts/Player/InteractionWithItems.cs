a2b265b8c66193cd8fbf993b4f608315cabc679b
Ildar Mindiiarov <AnMSLbR@users.noreply.github.com>
2025-03-22 22:53:16 +0500

    Изменения под тизер.

--- Assets/Scripts/Player/InteractionWithItems.cs
+++ Assets/Scripts/Player/InteractionWithItems.cs
@@ -7,21 +7,15 @@ public class InteractionWithItems : MonoBehaviour
     public float interactionDistance = 5f;
     public GameObject interactionUI;
     public TextMeshProUGUI interactionText;
-
-    private GameObject heldItem = null; // Текущий предмет в инвентаре
-
     void Update()
     {
-        if (heldItem != null)
-            PlaceItem(); // Если предмет уже в руках, ставим его перед персонажем
-        else
-            InteractionRay(); // Если предмета нет, пытаемся взять новый
+        if (isActivated is false)
+            InteractionRay();
     }
 
+    bool isActivated = false;
     private void InteractionRay()
     {
-        if (heldItem != null) return; // Если уже что-то держим, пропускаем
-
         Vector3 rayOrigin = playerTransform.position;
         Vector3 rayDirection = playerTransform.forward;
         RaycastHit hit;
@@ -38,25 +32,12 @@ public class InteractionWithItems : MonoBehaviour
 
                 if (Input.GetKeyDown(KeyCode.E))
                 {
-                    heldItem = hit.collider.gameObject;
-                    heldItem.SetActive(false); // Отключаем объект
+                    interactable.Interact();
                     hitSomething = false;
+                    isActivated = true;
                 }
             }
         }
         interactionUI.SetActive(hitSomething);
-    }
-
-    private void PlaceItem()
-    {
-        if (heldItem == null) return;
-
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            // Размещаем предмет перед персонажем
-            heldItem.transform.position = playerTransform.position + playerTransform.forward * 1.5f;
-            heldItem.SetActive(true);
-            heldItem = null; // Очищаем инвентарь
-        }
     }
 }
