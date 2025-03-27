6da620cf8c7de354eba6c8d3619042c6d8d4939b
Erazy-fs <erazy.fs@gmail.com>
2025-03-28 03:28:49 +0500

    fix

--- Assets/Scripts/TopDownControl.cs
+++ Assets/Scripts/TopDownControl.cs
@@ -33,66 +33,68 @@ public class TopDownControl : MonoBehaviour{
 
     private bool fireModeIsAuto = false;
     void Update() {
-
-        if (transform.position.y < -1) {
-            narrator.PlayerDeath();
-        }
-        isGrounded = controller.isGrounded;
-
-        if (isGrounded && velocity.y < 0) 
-            velocity.y = -2f;
-
-        var leftButton  = Input.GetMouseButton(0);
-        var rightButton = Input.GetMouseButton(1);
-
-        if (canMove) {
-            float moveHorizontal = Input.GetAxis("Horizontal");
-            float moveVertical   = Input.GetAxis("Vertical");
-
-            var move = new Vector3(moveHorizontal, 0, moveVertical);
-
-            if (move != Vector3.zero) {
-                if (leftButton || rightButton) {
-                    controller.Move(move * moveSpeed * Time.deltaTime);
-                } else {
-                    var toRotation = Quaternion.LookRotation(new Vector3(move.x, 0, move.z), Vector3.up);
-                    transform.rotation = Quaternion.Slerp(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
-                    controller.Move(transform.forward * moveSpeed * Time.deltaTime);
-                }
-                animator.SetBool("isWalking", true);
-            } else animator.SetBool("isWalking", false);
-
-            if (Input.GetButtonDown("Jump") && isGrounded)
-                velocity.y = Mathf.Sqrt(jumpForce * -2f * Physics.gravity.y * gravityScale);
-        } else LookAtMouse();
-
-
-        if (canShoot) {
-            if (leftButton || rightButton) {
-                LookAtMouse();
-                var wait = !animator.GetBool("isHolding");
-                animator.SetBool("isHolding", true);
-                gun.Shoot(wait, rightButton);
-            } else {
-                animator.SetBool("isHolding", false);
-                gun.StopShooting();
+        if (!GameManager.IsPaused())
+        {
+            if (transform.position.y < -1) {
+                narrator.PlayerDeath();
             }
-        }
+            isGrounded = controller.isGrounded;
 
-        velocity.y += Physics.gravity.y * gravityScale * Time.deltaTime;
-        controller.Move(new Vector3(0, velocity.y, 0) * Time.deltaTime);
+            if (isGrounded && velocity.y < 0) 
+                velocity.y = -2f;
+
+            var leftButton  = Input.GetMouseButton(0);
+            var rightButton = Input.GetMouseButton(1);
+
+            if (canMove) {
+                float moveHorizontal = Input.GetAxis("Horizontal");
+                float moveVertical   = Input.GetAxis("Vertical");
+
+                var move = new Vector3(moveHorizontal, 0, moveVertical);
+
+                if (move != Vector3.zero) {
+                    if (leftButton || rightButton) {
+                        controller.Move(move * moveSpeed * Time.deltaTime);
+                    } else {
+                        var toRotation = Quaternion.LookRotation(new Vector3(move.x, 0, move.z), Vector3.up);
+                        transform.rotation = Quaternion.Slerp(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+                        controller.Move(transform.forward * moveSpeed * Time.deltaTime);
+                    }
+                    animator.SetBool("isWalking", true);
+                } else animator.SetBool("isWalking", false);
+
+                if (Input.GetButtonDown("Jump") && isGrounded)
+                    velocity.y = Mathf.Sqrt(jumpForce * -2f * Physics.gravity.y * gravityScale);
+            } else LookAtMouse();
 
 
-        //Z.E.U.S.
-        if (canCallZEUS) {
-            if (Input.GetKeyDown(KeyCode.F)){
-                if (Physics.Raycast(transform.position, Vector3.down, out var hit, 3)) {
-                    var obj = hit.collider.gameObject;
-                    var plot = obj.GetComponent<PlotControl>();
-                    if (plot is not null)
-                        if (!plot.isComplete)
-                            groundControl.CallInZEUS(hit.point, plot.colorIndex);
-                } 
+            if (canShoot) {
+                if (leftButton || rightButton) {
+                    LookAtMouse();
+                    var wait = !animator.GetBool("isHolding");
+                    animator.SetBool("isHolding", true);
+                    gun.Shoot(wait, rightButton);
+                } else {
+                    animator.SetBool("isHolding", false);
+                    gun.StopShooting();
+                }
+            }
+
+            velocity.y += Physics.gravity.y * gravityScale * Time.deltaTime;
+            controller.Move(new Vector3(0, velocity.y, 0) * Time.deltaTime);
+
+
+            //Z.E.U.S.
+            if (canCallZEUS) {
+                if (Input.GetKeyDown(KeyCode.F)){
+                    if (Physics.Raycast(transform.position, Vector3.down, out var hit, 3)) {
+                        var obj = hit.collider.gameObject;
+                        var plot = obj.GetComponent<PlotControl>();
+                        if (plot is not null)
+                            if (!plot.isComplete)
+                                groundControl.CallInZEUS(hit.point, plot.colorIndex);
+                    } 
+                }
             }
         }
     }
